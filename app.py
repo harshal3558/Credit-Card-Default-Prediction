b9c22e5859361ad5b82c9ac54bc64cceb2b9a301
Harshal Patil <harshal3558@gmail.com>
2025-01-22 19:23:19 +0530

    Commenced Data Ingestion Step (2)

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
 
     try:
         # data_ingestion=data_ingestion()
-        data_ingestion_config = DataIngestionConfig()
+        # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
--- src/CCDP/components/data_ingestion.py
+++ src/CCDP/components/data_ingestion.py
@@ -11,9 +11,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path = os.path.join('artifacts','train.csv')
-    test_data_path = os.path.join('artifacts','test.csv')
-    raw_data_path = os.path.join('artifacts','raw.csv')
+    train_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str = os.path.join('artifacts','raw.csv')
 
 
 class DataIngestion:
