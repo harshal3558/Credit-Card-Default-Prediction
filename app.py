864f225776856b7e5f6c597b4ab4c2cc56f522ab
Harshal Patil <harshal3558@gmail.com>
2025-01-23 15:11:32 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src.CCDP.exception import CustomException
 # from src.CCDP.components import data_ingestion
 from src.CCDP.components.data_ingestion import DataIngestion
 from src.CCDP.components.data_ingestion import DataIngestionConfig
+from src.CCDP.components.data_transformation import DataTransformationConfig
+from src.CCDP.components.data_transformation import DataTransformation
 import sys
 
 if __name__ == "__main__":
@@ -11,8 +13,16 @@ if __name__ == "__main__":
     try:
         # data_ingestion=data_ingestion()
         # data_ingestion_config = DataIngestionConfig()
+        
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        # data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        # data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
