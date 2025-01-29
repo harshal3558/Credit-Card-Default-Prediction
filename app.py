eb595bdbc69cbac39e8214da8dccee7a7b354064
Harshal Patil <harshal3558@gmail.com>
2025-01-29 19:26:44 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from src.CCDP.logger import logging
 from src.CCDP.exception import CustomException
+from src.CCDP.components.data_ingestion import DataIngestion
+from src.CCDP.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
-        a=1/0
+        # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
